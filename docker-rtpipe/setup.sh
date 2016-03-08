d5e28701f204f7c09ce2dc3698fa65cdc0472b8d
Casey Law <caseyjlaw@gmail.com>
2016-03-08 14:16:12 -0800

    reordered build to make it faster on rebuilding

--- docker-rtpipe/setup.sh
+++ docker-rtpipe/setup.sh
@@ -2,6 +2,4 @@ apt-get update && apt-get install -y gcc groff
 conda install -y ncurses
 pip install awscli boto3 click
 
-chmod +x /entrypoint.sh
-chmod +x /control.py
 rm /setup.sh # self-destruct!
