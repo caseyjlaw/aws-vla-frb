2c80be913d59283ee3b327579ead2051770238c6
Jun Tan <Jun Tan>
2016-04-04 14:48:51 -0700

    add more explanation of the code

--- aws/search-data/dockerrun.py
+++ aws/search-data/dockerrun.py
@@ -2,6 +2,14 @@ import subprocess
 import sys
 import csv
 
+dockerMachine_name = str(sys.argv[1]) #search
+listsdms_txt = str(sys.argv[2]) #listsdms.txt
+#only run one of the tests below at a time
+readin(dockerMachine_name, listsdms_txt)
+#test_single_file_scan()
+#test_filter_target()
+
+
 def readin(dockerMachine_name, listsdms_txt):
      subprocess.call("export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)", shell=True)
      subprocess.call("docker-machine create " + strname + " --driver virtualbox", shell=True)
@@ -27,44 +35,40 @@ def readin(dockerMachine_name, listsdms_txt):
                                         line = line.split(",")
                                         targetWriter.writerow([line[0]] + [line[1]] + [line[2]]) 
 
+def test_single_file_scan():
+     '''the below section is JUST for testing the listscan command with the sample listsdms.txt we have at this point'''
+     filename = str(sys.argv[1])
+     with open(filename, "r+") as outfile:
+         sdmName_list = []
+         
+         #Turning the content in the listsdms.txt in to a list of sdm file names. 
+         for line in outfile:
+             sdmName_list = line.replace("[", "").replace("]", "").replace("u", "").replace("'", "").split(",")
+             
+         #do the list scan for each single sdm file and put the result into <sdmName>-target.csv
+         for sdmName in sdmName_list:
+               if ("gains" not in sdmName):
+                     temp_txt = sdmName + ".txt"
+                     target_csv = sdmName + "-target.csv"
+                     subprocess.call("docker run --rm $config caseyjlaw/rtpipe-aws listscans " + sdmName + " > " + temp_txt, shell=True)
+                     with open(temp_txt, "r") as tempFile, open(target_csv, "w+") as targetFile:
+                           targetWriter = csv.writer(targetFile, lineterminator = "\n")
+                           targetWriter.writerow(["scan number"] + ['type'] + ["size"])
+                           for line in tempFile:
+                                if ("TARGET" in line):
+                                     line = line.split(",")
+                                     targetWriter.writerow([line[0]] + [line[1]] + [line[2]])
 
 
-'''dockerMachine_name = str(sys.argv[1]) #search
-listsdms_txt = str(sys.argv[2]) #listsdms.txt
-readin(dockerMachine_name, listsdms_txt)'''
-
-'''the below section is JUST for testing the listscan command, we already have the sample listsdms.txt at this point
-   Comment out the following part if you decide to run the readin(...) function. '''
-
-filename = str(sys.argv[1])
-with open(filename, "r+") as outfile:
-    sdmName_list = []
-    
-    #Turning the content in the listsdms.txt in to a list of sdm file names. 
-    for line in outfile:
-        sdmName_list = line.replace("[", "").replace("]", "").replace("u", "").replace("'", "").split(",")
-        
-    #do the list scan for each single sdm file and put the result into <sdmName>-target.csv
-    for sdmName in sdmName_list:
-          if ("gains" not in sdmName):
-                temp_txt = sdmName + ".txt"
-                target_csv = sdmName + "-target.csv"
-                subprocess.call("docker run --rm $config caseyjlaw/rtpipe-aws listscans " + sdmName + " > " + temp_txt, shell=True)
-                with open(temp_txt, "r") as tempFile, open(target_csv, "w+") as targetFile:
-                      targetWriter = csv.writer(targetFile, lineterminator = "\n")
-                      for line in tempFile:
-                            if ("TARGET" in line):
-                                  targetWriter.writerow([line])
-
-
-'''The below section is Just for testing filtering process for the temp_txt output file
-   comment out this part to run readin(...) function.'''
-with open("doc.txt", "r") as tempFile, open("target.csv", "w+") as targetFile:
-      targetWriter = csv.writer(targetFile, lineterminator = "\n")
-      targetWriter.writerow(["scan number"] + ['type'] + ["size"])
-      for line in tempFile:
-            if ("TARGET" in line):
-                  line = line.split(",")
-                  targetWriter.writerow([line[0]] + [line[1]] + [line[2]])                               
+def test_filter_target():
+     '''The below section is Just for testing target-filtering process for the doc.txt, the sample output file
+        from the listscan'''
+     with open("doc.txt", "r") as tempFile, open("target.csv", "w+") as targetFile:
+           targetWriter = csv.writer(targetFile, lineterminator = "\n")
+           targetWriter.writerow(["scan number"] + ['type'] + ["size"])
+           for line in tempFile:
+                 if ("TARGET" in line):
+                       line = line.split(",")
+                       targetWriter.writerow([line[0]] + [line[1]] + [line[2]])                               
                                   
                 
