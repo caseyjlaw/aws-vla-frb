4c736b89629eb7b1fbd8c03007293c0a7814a616
Casey Law <caseyjlaw@gmail.com>
2016-05-02 15:51:00 -0700

    activegit included. updated base.ipynb to use ag/ML

--- docker-rtpipe/setup.sh
+++ docker-rtpipe/setup.sh
@@ -1,5 +1,6 @@
 apt-get update && apt-get install -y gcc groff
 conda install -y ncurses
-pip install awscli boto3 click
+pip install awscli boto3 click activegit
+cd / && git clone http://github.com/caseyjlaw/alnotebook.git
 
 rm /setup.sh # self-destruct!
