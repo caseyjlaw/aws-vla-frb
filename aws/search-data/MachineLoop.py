1817603587995002391a30ec02a16acca4c9d0dc
JunTan <semou9325@gmail.com>
2016-05-31 00:08:25 -0700

    modify to create with amazon ec2 instance

--- aws/search-data/MachineLoop.py
+++ aws/search-data/MachineLoop.py
@@ -7,7 +7,7 @@ numOfMachine = 1
 machineBaseName = 'machine'
 for i in range(numOfMachine):
     machineName = machineBaseName + str(i)
-    subprocess.call("docker-machine create " + machineName + " --driver virtualbox", shell=True)
+    subprocess.call("docker-machine create " + machineName + " --driver amazonec2 --amazonec2-region us-west-2 --amazonec2-instance-type c4.xlarge --amazonec2-root-size 256 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY", shell=True)
     nameList.append(machineName)
 
 for name in nameList:
