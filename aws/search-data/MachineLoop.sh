ed4834f0e7f434f628edda0c04f3d3c7b58b7980
JunTan <semou9325@gmail.com>
2016-05-31 18:07:01 -0700

    convert python script to bash script, modify numOfMachine to create the amount of machines you need

---
+++ aws/search-data/MachineLoop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+numOfMachine=2
+
+function SearchScanLoop() {
+	echo into the search
+	eval $(docker-machine env $machineName)
+	export config="-m 7G -p 8888:8888 -v /home/ubuntu:/work -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
+
+	count=`docker ps -a |grep Up | wc -l`
+	if (($count < 1)); then
+	    git pull
+	    sdmfile=`python next_to_search_sdm.py`
+	    scan=`python next_to_search_scan.py`
+	    python add_finished_to_complete.py $sdmfile $scan
+	    git commit -am 'starting a job'
+	    git push
+	    contid=`docker run -d $config caseyjlaw/rtpipe-aws search $sdmfile $scan`
+	else
+		echo machine is in use
+	fi
+}
+
+python createMachine.py $numOfMachine
+
+for i in $(more machineList.csv); do
+	SearchScanLoop $i
+done
