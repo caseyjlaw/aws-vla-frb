17daa587157ca2bc7f264af1069f73b873a0e472
Casey Law <caseyjlaw@gmail.com>
2016-06-03 09:53:14 -0700

    adding emacs and x11-apps for display testing

--- docker-rtpipe/setup.sh
+++ docker-rtpipe/setup.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y gcc groff
+apt-get update && apt-get install -y gcc groff emacs x11-apps
 conda install -y ncurses
 pip install awscli boto3 click activegit
 cd / && git clone http://github.com/caseyjlaw/alnotebook.git
