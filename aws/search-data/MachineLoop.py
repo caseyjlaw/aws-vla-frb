cc5a2b642d737c8e4d472592ce70f21d3c2afa5a
Jun Tan <Jun Tan>
2016-06-10 10:03:17 -0700

    fix small bug

--- aws/search-data/MachineLoop.py
+++ aws/search-data/MachineLoop.py
@@ -7,7 +7,7 @@ print("start process")
 assert (len(sys.argv) == 2), "Missing the machine base name. Format: python MachineLoop.py <machineBaseName>"
 nameList = []
 numOfMachine = 3
-machineBaseName = str(sys.argv[2])
+machineBaseName = str(sys.argv[1])
 for i in range(numOfMachine):
     machineName = machineBaseName + str(i)
     subprocess.call("docker-machine create " + machineName + " --driver amazonec2 --amazonec2-region us-west-2 --amazonec2-instance-type c4.xlarge --amazonec2-root-size 256 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY", shell=True)
