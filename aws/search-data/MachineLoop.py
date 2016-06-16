f64ea234d97eebff7a5ab83b5ff3ab09603599a1
Jun Tan <semou9325@gmail.com>
2016-06-16 12:37:12 -0700

    starting a job

--- aws/search-data/MachineLoop.py
+++ aws/search-data/MachineLoop.py
@@ -3,7 +3,7 @@ import os.path
 import sys
 import time
 '''
-python MachineLoop.py <machineBaseName> <numOfMachine> [<spotPrice> <spotPriceMode>]
+python MachineLoop.py <machineBaseName> <numOfMachine> [<spotPrice>]
 '''
 print("start process")
 assert (len(sys.argv) >= 3), "Missing the machine base name. Format: python MachineLoop.py <machineBaseName> <numOFMachine>"
@@ -12,11 +12,9 @@ machineBaseName = str(sys.argv[1])
 numOfMachine = int(sys.argv[2])
 spotPriceMode = False
 
-if (len(sys.argv) == 5 and str(sys.argv[4])==True):
+if (len(sys.argv) == 4):
     spotPrice = str(sys.argv[3])
     spotPriceMode = True
-else:
-    assert (len(sys.argv) == 5 and str(sys.argv[4])==True), "Missing arguments or wrong arguments"
 
 for i in range(numOfMachine):
     machineName = machineBaseName + str(i)
@@ -24,7 +22,6 @@ for i in range(numOfMachine):
         subprocess.call("docker-machine create --driver amazonec2 --amazonec2-region us-west-2 --amazonec2-instance-type c4.xlarge --amazonec2-root-size 256 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY --amazonec2-request-spot-instance --amazonec2-spot-price " + spotPrice + " " + machineName, shell=True)
     else:
         subprocess.call("docker-machine create " + machineName + " --driver amazonec2 --amazonec2-region us-west-2 --amazonec2-instance-type c4.xlarge --amazonec2-root-size 256 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY", shell=True)
-
     nameList.append(machineName)
     time.sleep(3)
 
