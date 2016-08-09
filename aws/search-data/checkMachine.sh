bf2477612157b398b70b42a7c722bf08f2a62a59
Casey Law <caseyjlaw@gmail.com>
2016-08-09 15:59:42 -0700

    error state check works with awk

--- aws/search-data/checkMachine.sh
+++ aws/search-data/checkMachine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 machineext=`docker-machine ls | awk '{ print $1 }' | grep $machineName$ | wc -l`
-error=`docker-machine ls | awk '{ print $1 }' | grep $machineName$ | grep Error | wc -l`
+error=`docker-machine ls | awk '{ print $1$4 }' | grep ${machineName}Error | wc -l`
 echo Status of $machineName: $machineext $error
 if (($machineext == 1)); then
     if (($error > 0)); then
